a5c3c9f15c0cad58e931f02348c7b268eee3d596
Hakim-Mate <kim.k866@gmail.com>
2015-11-27 14:40:56 +0100

    resolve conflit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Basicperf\MetricsBundle\BasicperfMetricsBundle(),
             new HexaUserBundle\HexaUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
